d3aaae4b4c2f04df3e8d195f5a92d986f4c7b4c7
Julien Bidoret <julienbidoret@gmail.com>
2015-05-20 05:55:15 +0200

    Exemples

---
+++ exemples/03/script.js
@@ -0,0 +1,11 @@
+
+var dis_bonjour = function(a_qui, sur_quel_ton){
+    if (sur_quel_ton == "reverencieux") {
+        alert ("Mes cordiales salutations, " + a_qui + " !");
+    } else if (sur_quel_ton == "familier") {
+        alert ("Salut " + a_qui + " !");
+    } else {
+        alert ("Yo " + a_qui + " !");
+    }
+}
+
---
+++ exemples/04/script.js
@@ -0,0 +1,16 @@
+// Créer une liste de trois personnes
+var liste = []
+
+// Créer deux nouvelles personnes et les ajouter à la liste
+
+// Supprimer la dernière (les derniers ont toujours tort)
+
+// Créer une fonction qui permette à une personne de se présenter, 
+// en affichant par exemple : “Bonjour, je m’appelle Julien”
+// Cette fonction prendra un paramètre : le nom de la personne
+
+function introduce(name){
+	//
+}
+
+// Créer une boucle qui invoque la fonction “introduce” pour chaque personne de la liste
