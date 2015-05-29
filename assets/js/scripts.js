bc6e8edadc9ee8c190e0e6d9c0c25d2fb159b918
Julien Bidoret <julienbidoret@gmail.com>
2015-05-29 09:47:53 +0200

    Ajout table des matières

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,1 +1,16 @@
+  $(function(){ 
+  	var $menulink = $('<span id="menulink">☰</span>');
+  	$menulink.on('click', function(){
+  		$('body').toggleClass('open-toc');
+  	})
+  	$("#toc").before($menulink);
+    $("#toc").tableOfContents(
+      $("#wrapper"),      // Scoped to div#wrapper
+      {
+        startLevel: 1,    // H2 and up
+        depth:      3,    // H2 through H4,
+        //topLinks:   true, // Add "Top" Links to Each Header
+      }
+    ); 
+  });
 
